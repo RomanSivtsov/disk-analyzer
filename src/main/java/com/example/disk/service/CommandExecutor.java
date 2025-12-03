29145025b47bda901fdb23a03aa5b154d0304cd9
RomanSivtsov <tvgustav@gmail.com>
2025-12-03 21:49:28 +0300

    Add description #2

--- src/main/java/com/example/disk/service/CommandExecutor.java
+++ src/main/java/com/example/disk/service/CommandExecutor.java
@@ -7,5 +7,13 @@ import java.util.List;
  * Интерфейс для выполнения системных команд в терминале ОС.
  */
 public interface CommandExecutor {
+
+    /**
+     * Выполняет переданную команду и возвращает результат выполнения.
+     * @param command строка команды (например, "df -h")
+     * @return список строк, которые команда вывела в консоль
+     * @throws IOException если произошла ошибка ввода-вывода
+     * @throws InterruptedException если поток был прерван во время ожидания
+     */
     List<String> execute(String command) throws IOException, InterruptedException;
 }
