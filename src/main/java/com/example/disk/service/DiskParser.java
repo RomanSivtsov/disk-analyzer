4dab9a273ad28e9c891289be88f25a22f6ef3b0e
RomanSivtsov <tvgustav@gmail.com>
2025-12-03 21:49:49 +0300

    Add description to DiskParser #1

--- src/main/java/com/example/disk/service/DiskParser.java
+++ src/main/java/com/example/disk/service/DiskParser.java
@@ -4,5 +4,11 @@ import com.example.disk.model.DiskStat;
 import java.util.List;
 
 public interface DiskParser {
+
+    /**
+     * Парсит список строк, полученных от системной утилиты.
+     * @param commandOutput список строк (вывод консоли)
+     * @return список объектов DiskStat с информацией о дисках
+     */
     List<DiskStat> parse(List<String> commandOutput);
 }
