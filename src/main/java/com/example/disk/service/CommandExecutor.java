9e860023d2505a761ec1546b4d1e41890512af87
RomanSivtsov <tvgustav@gmail.com>
2025-12-03 21:49:15 +0300

    Add description #1

--- src/main/java/com/example/disk/service/CommandExecutor.java
+++ src/main/java/com/example/disk/service/CommandExecutor.java
@@ -3,6 +3,9 @@ package com.example.disk.service;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Интерфейс для выполнения системных команд в терминале ОС.
+ */
 public interface CommandExecutor {
     List<String> execute(String command) throws IOException, InterruptedException;
 }
